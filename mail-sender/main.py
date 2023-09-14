f90338dbe7f0d830d846d4b354dcb8315c8a9cd8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-14 17:35:26 +0530

    Create main.py

---
+++ mail-sender/main.py
@@ -0,0 +1,15 @@
+def build_message(destination, obj, body, attachments=[]):
+    if not attachments: # no attachments given
+        message = MIMEText(body)
+        message['to'] = destination
+        message['from'] = our_email
+        message['subject'] = obj
+    else:
+        message = MIMEMultipart()
+        message['to'] = destination
+        message['from'] = our_email
+        message['subject'] = obj
+        message.attach(MIMEText(body))
+        for filename in attachments:
+            add_attachment(message, filename)
+    return {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
