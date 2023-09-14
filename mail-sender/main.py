014ec5d3d0b10caa57153a6f8a1bf379989c5eb5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-14 17:36:32 +0530

    Update main.py

--- mail-sender/main.py
+++ mail-sender/main.py
@@ -31,3 +31,31 @@ def send_message(service, destination, obj, body, attachments=[]):
       userId="me",
       body=build_message(destination, obj, body, attachments)
     ).execute()
+
+
+# Adds the attachment with the given filename to the given message
+def add_attachment(message, filename):
+    content_type, encoding = guess_mime_type(filename)
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(filename, 'rb')
+        msg = MIMEText(fp.read().decode(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(filename, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(filename, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(filename, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    filename = os.path.basename(filename)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
