c35eaee9b79470113f98dbc4ea7bf9241b164bbe
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-14 17:42:09 +0530

    Update __init__.py

--- mail-sender/__init__.py
+++ mail-sender/__init__.py
@@ -26,7 +26,7 @@ def gmail_authenticate():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('..secrets/credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('../secrets/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # save the credentials for the next run
         with open("token.pickle", "wb") as token:
