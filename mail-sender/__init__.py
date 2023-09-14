137b367abf3c1b055dd9addf1a9ade6d6c85b955
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-14 17:29:48 +0530

    Update __init__.py

--- mail-sender/__init__.py
+++ mail-sender/__init__.py
@@ -6,7 +6,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 # for encoding/decoding messages in base64
 from base64 import urlsafe_b64decode, urlsafe_b64encode
-# for dealing with attachement MIME types
+# for dealing with attachment MIME types
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
