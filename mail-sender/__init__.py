04a145f4b6a5dfc30bbb161e9d5e4a8d8d4512e0
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-14 17:29:40 +0530

    Update __init__.py

--- mail-sender/__init__.py
+++ mail-sender/__init__.py
@@ -1,4 +1,22 @@
+import os
+import pickle
+# Gmail API utils
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+# for encoding/decoding messages in base64
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+# for dealing with attachement MIME types
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from mimetypes import guess_type as guess_mime_type
 
+# Request all access (permission to read/send/receive emails, manage the inbox, and more)
+SCOPES = ['https://mail.google.com/']
+our_email = 'your_gmail@gmail.com'
 
 
 def gmail_authenticate():
