2b609d6dc77a71d3edcbfb9644074ca9395a4796
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-14 17:35:38 +0530

    Update main.py

--- mail-sender/main.py
+++ mail-sender/main.py
@@ -13,3 +13,10 @@ def build_message(destination, obj, body, attachments=[]):
         for filename in attachments:
             add_attachment(message, filename)
     return {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
+
+
+def send_message(service, destination, obj, body, attachments=[]):
+    return service.users().messages().send(
+      userId="me",
+      body=build_message(destination, obj, body, attachments)
+    ).execute()
